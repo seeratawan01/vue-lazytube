10f125ea5b7a70661758cfaebba50775238def5e
seeratawan01 <seeratsdsking@gmail.com>
2021-03-20 16:38:28 +0500

    Watch Loaded

--- src/mixins/youtubeMixin.js
+++ src/mixins/youtubeMixin.js
@@ -77,7 +77,9 @@ export default {
                      <svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%">
                      <path class="ly-large-play-button-bg" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f00"></path><path d="M 45,24 27,14 27,34" fill="#fff"></path></svg>
                       </button>
-                  </a>`;
+                  </a>
+    
+                    `;
         },
         calcAspect (aspect) {
             let aspects = aspect.split(':')
@@ -86,6 +88,45 @@ export default {
             } else {
                 return aspects[1]/aspects[0] * 100
             }
+        },
+        iframeURLChange(iframe, callback) {
+            var lastDispatched = null;
+            // var oldDoc = null;
+
+            var dispatchChange = function () {
+                // const propser= {...iframe.contentWindow.location}
+                // console.log(iframe.contentWindow.location, lastDispatched)
+                // callback(propser);
+                var newHref = iframe.contentDocumentf;
+
+
+                if (newHref !== lastDispatched) {
+                    callback(newHref, lastDispatched);
+                    lastDispatched = newHref;
+                }
+            };
+
+            var unloadHandler = function () {
+                // Timeout needed because the URL changes immediately after
+                // the `unload` event is dispatched.
+                setTimeout(dispatchChange, 0);
+            };
+
+            function attachUnload() {
+                // Remove the unloadHandler in case it was already attached.
+                // Otherwise, there will be two handlers, which is unnecessary.
+                iframe.contentWindow.removeEventListener("unload", unloadHandler);
+                iframe.contentWindow.addEventListener("unload", unloadHandler);
+            }
+
+            iframe.addEventListener("load", function () {
+                attachUnload();
+
+                // Just in case the change wasn't dispatched during the unload event...
+                dispatchChange();
+            });
+
+            attachUnload();
         }
     }
 };
